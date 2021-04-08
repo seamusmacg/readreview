fce7ad83f8a3c6e69b7b970277cdcf160ad8aeb3
Seamus <seamus.macgrianna12@gmail.com>
2021-04-08 10:12:03 +0100

    Restructure directories

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
-from flask import Flask
 import os
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
-  import env
+    import env
 
 # Create instance of Flask class 
 app = Flask(__name__)
 
+# Configure MongoDB and Secret Key
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY") 
+
+mongo = PyMongo(app)
+
 
 # Decorator telling Flask which URL to trigger
 @app.route("/")
-def test():
-  return "Application is up and running! Testing Git!"
+def index():
+  books = mongo.db.books.find()
+  return render_template("index.html", books=books)
 
 
 if __name__ == "__main__":
---
+++ env.py
@@ -0,0 +1,9 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", ")H@McQfThWmZq4t7w!z%C*F-JaNdRgUk")
+os.environ.setdefault("MONGO_URI","mongodb+srv://admin:rHo1bLMEFb6Al9FIzh3Y@mycluster.zpxni.mongodb.net/read_review?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_DBNAME", "read_review")
+
+
