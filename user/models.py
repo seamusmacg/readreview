b70b8385f1e0bd094435aabed4000185300b31a8
Seamus <seamus.macgrianna12@gmail.com>
2021-04-19 17:43:26 +0100

    Update create_user method

--- user/models.py
+++ user/models.py
@@ -1,14 +1,23 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify, request
+#from passlib.hash import pbkdf2_sha256
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import *
 
 class User:
 
-  def register(self):
+  def create_user(self):
+    print(request.form)
     
+    # Create the user object
     user = {
-      "_id": "",
-      "name": "",
-      "password": ""
+      "username": request.form.get('username'),
+      "password": request.form.get('password')
     }
 
-    return jsonify(user), 200
+    # Encrypt  the password
+    user['password'] = generate_password_hash(user['password'])
+
+
+    mongo.db.users.insert_one(user)
+
+    return jsonify(user)
