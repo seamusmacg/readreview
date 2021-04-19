9dde2e21afcaaaf0cbb5119018578ef1d65d8900
Seamus <seamus.macgrianna12@gmail.com>
2021-04-19 17:40:34 +0100

    Add jquery for form submission

--- static/js/script.js
+++ static/js/script.js
@@ -7,3 +7,26 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$("form[name=register_account]").submit(function(e){
+
+  var $form = $(this);
+  var $error = $form.find(".error");
+  var data = $form.serialize();
+
+  $.ajax({
+    url: "/register",
+    type: "POST",
+    data: data,
+    dataType: "json",
+    success: function(resp) {
+      console.log(resp);
+    },
+    error: function(resp) {
+      console.log(resp);
+    }
+
+  })
+
+  e.preventDefault();
+});
